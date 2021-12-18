be5c5377f509bacf670d92fe3d51fc58c8d3c5df
aneme1038 <thriftman25@gmail.com>
2021-12-18 14:53:13 -0500

    app page adjustment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Foxhole Logistics</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
@@ -15,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Enter
         </a>
       </header>
     </div>
