4b253c5fe31b0c6aa5eca7dd4c9f76a98c89f6e1
aneme1038 <thriftman25@gmail.com>
2021-12-18 15:55:28 -0500

    adding authentication components and initial navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import {useAuth0} from '@auth0/auth0-react';
+import NavBar from './navigation/nav-bar';
+import Loading from './navigation/loading';
+import Home from './navigation/home';
 
-function App() {
+const App = () => {
+  const {isLoading} = useAuth0();
+
+  if (isLoading) {
+    return (
+      <Loading />
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Foxhole Logistics</h1>
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Enter
-        </a>
-      </header>
+    <div id="app" className="d-flex flex-column h-100">
+      <NavBar />
+      <div className="container flex-grow-1">
+        <Routes>
+          <Route path="/" exact component={Home} />
+        </Routes>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/navigation/main-nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainNav = () => {
+  return (
+    <div className="nav-container mb-3">
+        <h1>Navigation</h1>
+    </div>
+  );
+};
+
+export default MainNav;
